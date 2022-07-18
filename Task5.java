d05f8f4d298e1a70eba220eb61c62a7e07ba7ca2
Sabaritesting <108785595+Sabaritesting@users.noreply.github.com>
2022-07-18 12:52:55 +0530

    Add files via upload

---
+++ Task5.java
@@ -0,0 +1,18 @@
+package Tasks;
+
+import java.util.Scanner;
+
+public class Task5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter the first number");
+int a=sc.nextInt();
+System.out.println("Enter the second number");
+int b=sc.nextInt();
+System.out.println("The product of two numbers is "+a*b);
+
+	}
+
+}
