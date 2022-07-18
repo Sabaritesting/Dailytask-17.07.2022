4c096a9401568b365f8efacb1c9cbf37a60ab41c
Sabaritesting <108785595+Sabaritesting@users.noreply.github.com>
2022-07-18 12:54:46 +0530

    Add files via upload

---
+++ Task10.java
@@ -0,0 +1,10 @@
+package Tasks;
+
+public class Task10 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println(4.2*(1+(1.025/2)+(1.25/5)+(12.5/5)+(9.25/5)));
+	}
+
+}
