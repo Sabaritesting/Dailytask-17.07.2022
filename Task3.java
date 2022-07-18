a2e3b4aec529bfdad8a0280b7a050d1b70034d15
Sabaritesting <108785595+Sabaritesting@users.noreply.github.com>
2022-07-18 12:52:14 +0530

    Add files via upload

---
+++ Task3.java
@@ -0,0 +1,12 @@
+package Tasks;
+
+public class Task3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a=4524,b=24,c=a/b;
+System.out.println("The division of two numbers is "+c);
+
+	}
+
+}
