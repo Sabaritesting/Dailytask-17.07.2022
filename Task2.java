12a573d6280551458ca0ee5c331967e834474b9c
Sabaritesting <108785595+Sabaritesting@users.noreply.github.com>
2022-07-18 12:51:53 +0530

    Add files via upload

---
+++ Task2.java
@@ -0,0 +1,11 @@
+package Tasks;
+
+public class Task2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a=124,b=452;
+System.out.print("The sum of two numbers is "+a*b);
+	}
+
+}
