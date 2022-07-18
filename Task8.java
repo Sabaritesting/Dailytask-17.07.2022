ec05f362ff425d18425347b8625431f596a38fa4
Sabaritesting <108785595+Sabaritesting@users.noreply.github.com>
2022-07-18 12:54:02 +0530

    Add files via upload

---
+++ Task8.java
@@ -0,0 +1,14 @@
+package Tasks;
+
+public class Task8 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("*");
+System.out.println("**");
+System.out.println("***");
+System.out.println("****");
+System.out.println("*****");
+	}
+
+}
