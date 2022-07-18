58471267f67dd26e8e1f63dccf97815c984ae236
Sabaritesting <108785595+Sabaritesting@users.noreply.github.com>
2022-07-18 12:53:14 +0530

    Add files via upload

---
+++ Task6.java
@@ -0,0 +1,26 @@
+package Tasks;
+
+import java.util.Scanner;
+
+public class Task6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter the  first value");
+int a=sc.nextInt();
+System.out.println("Enter the second value");
+int b=sc.nextInt();
+int c=a+b;
+System.out.println("The Sum of the numbers are "+c);
+int d=a-b;
+System.out.println("The difference of two numbers are" +d);
+int e=a*b;
+System.out.println("The product of the numbers are"+e);
+int f=a/b;
+System.out.println("The division of two numbers are"+f);
+int h=a%b;
+System.out.println("The modulo of two numbers are"+h);
+	}
+
+}
